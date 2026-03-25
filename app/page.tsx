47d262934417bd7a8b080df872b89f0ec9e22e28
أَدْهَمُ بْنُ أَحْمَدَ <adhambinahmad@gmail.com>
2026-03-25 19:04:41 +0200

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { motion } from "framer-motion";
-import SocialCard from "./components/SocialCard.jsx";
+import SocialCard from "./components/SocialCard";
 
 export default function Home() {
   return (
